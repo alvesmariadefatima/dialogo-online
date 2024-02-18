778fded8287a190d797ae574957b18ca35661b11
Maria de Fátima <mnunesalves334@gmail.com>
2024-02-18 15:21:51 -0300

    Ajustes finais para deixar as informações das salas criadas salvas localmente usando localStorage

--- src/components/pages/SalasExistentes.jsx
+++ src/components/pages/SalasExistentes.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './SalasExistentes.module.css';
-import { Link, useLocation } from 'react-router-dom'; // Importando useLocation para acessar os dados passados via navegação
+import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 
 function SalasExistentes() {
-    const location = useLocation();
-    const { nomeSala, descricao } = location.state || {};
+    const [salas, setSalas] = useState([]);
+
+    useEffect(() => {
+        const carregarSalas = () => {
+            try {
+                const savedSalas = localStorage.getItem('salasExistentes');
+                if (savedSalas) {
+                    setSalas(JSON.parse(savedSalas));
+                }
+            } catch (error) {
+                console.error('Erro ao carregar salas:', error.message);
+            }
+        };
+
+        carregarSalas();
+    }, []);
 
     return (
         <div>
             <Navbar />
             <h1 className={styles.txtsalasexistentes}>Salas Existentes</h1>
-            <p className={styles.txtsalasexistentes}>Junte-se as salas existentes e conheça novas pessoas 🚪</p>
+            <p className={styles.txtsalasexistentes}>Junte-se às salas de bate-papo existentes e conheça novas pessoas 🚪</p>
             <div className={styles.containerButton}>
                 <Link to="/SalaEscolherChat">
                     <button type="submit" className={styles["send-back"]}>
@@ -21,15 +35,15 @@ function SalasExistentes() {
             </div>
 
             <div className={styles.containerSalasExistentes}>
-                {nomeSala && descricao && ( // Renderizar apenas se as informações da sala existirem
-                    <section>
-                        <p className={styles.txtsala}>Nome da Sala: {nomeSala}</p>
-                        <p className={styles.txtdescricao}>Descrição: {descricao}</p>
+                {salas.map((sala, index) => (
+                    <section key={index}>
+                        <p className={styles.txtsala}>Nome da Sala: {sala.nome}</p>
+                        <p className={styles.txtdescricao}>Descrição: {sala.descricao}</p>
                         <Link to="/Chat" className={styles.linkEntrar} style={{textDecoration: 'none'}}>
                             <button className={styles.btnEntrarSala}>Entrar</button>
                         </Link>
                     </section>
-                )}
+                ))}
             </div>
         </div>
     );
